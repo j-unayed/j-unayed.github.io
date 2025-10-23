0494109e6becbd0dafc8fb060e4f85ad00f24cbc
j-unayed <92309342+j-unayed@users.noreply.github.com>
2025-10-23 23:18:35 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-let slideIndex = 0;
-let slides = document.getElementsByClassName("slide");
-showSlides();
-
-// Next/previous controls
-function plusSlides(n) {
-  slideIndex += n;
-  if(slideIndex >= slides.length) slideIndex = 0;
-  if(slideIndex < 0) slideIndex = slides.length - 1;
-  updateSlides();
-}
-
-// Automatic slideshow
-function showSlides() {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
-
-function updateSlides() {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex].style.display = "block";
-}
 // Hero manual slideshow
 let heroIndex = 0;
 const heroSlides = document.querySelectorAll(".hero-slideshow .slide");
